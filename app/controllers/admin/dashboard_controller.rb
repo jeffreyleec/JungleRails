682edb416f7173663593a84e1a0d19c580c6a072
Jeffrey Lee <jeffrey@JeffreysMacBook.hitronhub.home>
2022-09-22 16:05:44 -0400

    controllers updated

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @category = Category.find(params[:id])
+    @products = @category.products.order(created_at: :desc)
   end
 end
